fc4d476eba62e690ad6e82da98ab7492f7eb87db
Ethan Woodward <Ethan.Woodward@gmail.com>
2014-01-03 22:37:42 -0500

    Initial commit for the Copy Note Chrome Extension

---
+++ bg.js
@@ -0,0 +1,137 @@
+// Background script to run persistently 
+// while the browser is running.
+
+var cnDB;
+var request = indexedDB.open('cnDB', 1);
+var notes = new Array();
+
+/* TEMPLATE FOR NOTE OBJECTS */
+// var note = {
+// 	text: "",
+// 	id: "",
+// 	url: ""
+// };
+
+request.onsuccess = function(e) {
+	cnDB = request.result;
+	loadNotesFromDB();
+	console.log(cnDB);
+};
+
+request.onerror = function(e) {
+	console.log(e.srcElement.error.message);
+};
+
+request.onupgradeneeded = function(e) {
+
+	var db = e.target.result;
+	console.log(db);
+
+	var objectStore = db.createObjectStore('cnDBStore', {autoIncrement: true});
+
+};
+
+function transComplete(e) {
+	console.log("cnDB transaction complete");
+};
+
+function transError(e) {
+	console.error("Error in cnDB transaction");
+	console.log(e);
+};
+
+/************************************* 
+  Functions for adding notes to the DB
+**************************************/
+
+function addNoteToDB(note) {
+
+	var transaction = cnDB.transaction(['cnDBStore'], 'readwrite');
+
+	transaction.oncomplete = transComplete;
+	transaction.onerror = transError;
+
+	var objectStore = transaction.objectStore('cnDBStore');
+	var request = objectStore.add(note);
+
+	request.onsuccess = function(e) {
+		console.log("Note added");
+	};
+
+	request.onerror = function(e) {
+		console.log("Error adding note");
+	};
+
+};
+
+// Creates a note from the text area in popup.html
+function createNoteFromPopup(text) {
+
+	var note = {
+		text: text,
+		id: "",
+		url: ""
+	};
+
+	addNoteToDB(note);
+
+};
+
+// Creates a note from right-clicking highlighted text
+function createNoteFromWebPage(info, tab) {
+
+	var note = {
+		text: info.selectionText,
+		id: "",
+		url: tab.url
+	};
+
+	console.log(note);
+	addNoteToDB(note);
+
+};
+
+/**************************************** 
+  Functions for getting notes from the DB
+****************************************/
+
+function getNotes() {
+	return notes;
+};
+
+function setNotes(e) { 
+
+	var cursor = e.target.result;
+
+	if (cursor) {
+
+		cursor.value.id = cursor.key;
+
+		notes.push(cursor.value);
+		console.log(cursor.value);
+
+		cursor.continue();
+
+	}
+
+};
+
+function loadNotesFromDB() {
+
+	var transaction = cnDB.transaction(['cnDBStore'], 'readonly');
+
+	transaction.oncomplete = transComplete;
+	transaction.onerror = transError;
+
+	var objectStore = transaction.objectStore('cnDBStore');
+	objectStore.openCursor(null, 'prev').onsuccess = setNotes;
+
+};
+
+// TODO: Create a remove/delete note function
+
+chrome.contextMenus.create({id: 'cnID', title: 'Copy Note', contexts: ['selection']}, function() {
+	console.log("Context Menu successfully created");
+});
+
+chrome.contextMenus.onClicked.addListener(createNoteFromWebPage);
---
+++ popup.js
@@ -0,0 +1,64 @@
+// Allows for access to BG script
+// Keeps DB transactions in one place
+var bgPage = chrome.extension.getBackgroundPage();
+var notes;
+
+function onImageClicked(e) {
+	console.log(e);
+}
+
+function createTextManipImage(src) {
+
+	var image = document.createElement('img');
+	image.setAttribute('src', src);
+
+	image.onClicked.addListener(onImageClicked);
+
+	return image;
+
+}
+
+function createNote(text) {
+
+	var note = document.createElement('p');
+	note.setAttribute('class', 'note');
+	note.appendChild(document.createTextNode(text));
+
+	return note;
+
+}
+
+function createNoteContainer(id) {
+
+	var div = document.createElement('div');
+	div.setAttribute('id', id);
+	div.setAttribute('class', 'noteContainer');
+
+	return div;
+
+}
+
+function addNoteToView(id, text) {
+
+	var parentDiv = document.getElementById("parentDiv");
+
+	var childDiv = createNoteContainer(id);
+	var note = createNote(text);
+
+	childDiv.appendChild(note);
+	parentDiv.appendChild(childDiv);
+
+}
+
+function addNotesToView() {
+
+	notes = bgPage.getNotes();
+	console.log(notes);
+
+	for (var i = 0; i < notes.length; i++) {
+		addNoteToView(notes[i].id, notes[i].text);
+	}
+
+}
+
+document.onload = addNotesToView();
