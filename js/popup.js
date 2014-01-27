af6138640161e8c26ee95527c566d20585f0c600
Takadimi <Ethan.Woodward@gmail.com>
2014-01-27 17:00:51 -0500

    Added a new js file to hold all of the event handlers for the popup

--- js/popup.js
+++ js/popup.js
@@ -2,71 +2,6 @@
 // Keeps DB transactions in one place
 var bgPage = chrome.extension.getBackgroundPage();
 
-/*** EVENT HANDLERS FOR ALL CLICK ACTIONS ***/
-function onCreateNoteButtonClicked(e) {
-	changeHeaderView('submit');
-}
-
-function onSubmitButtonClicked(e) {
-
-	var noteTextArea = document.getElementById('noteInput');
-	bgPage.createNoteFromPopup(noteTextArea.value);
-	changeHeaderView('home');
-
-}
-
-function onSaveButtonClicked(e) {
-
-	var noteTextArea = document.getElementById('editInput');
-	bgPage.updateNoteInDB(noteTextArea.getAttribute('data-noteid'), noteTextArea.value);
-	changeHeaderView('home');
-
-}
-
-function onCancelButtonClicked(e) {
-	changeHeaderView('home');
-}
-
-function onClearButtonClicked(e) {
-
-	var parent = e.target.parentElement;
-	bgPage.removeNoteFromDB(parent.id);
-
-	removeNoteFromView(parent.id);
-
-}
-
-function onLinkClicked(e) {
-	bgPage.openLinkClickedInPopup(e.target.href);
-}
-
-function onTextManipButtonClicked(e) {
-
-	var noteText = e.target.parentElement.firstChild;
-
-	if (e.target.id == 'more') {
-		noteText.style.height = "100%";
-		e.target.className = 'fa fa-chevron-up';
-		e.target.id = 'less';
-	} else if (e.target.id == 'less') {
-		noteText.style.height = "17px";
-		e.target.className = 'fa fa-chevron-down';
-		e.target.id = 'more';
-	}
-
-}
-
-function onEditButtonClicked(e) {
-	changeHeaderView('edit');
-
-	var noteTextArea = document.getElementById('editInput');
-	noteTextArea.value = e.target.parentElement.firstChild.innerText;
-	noteTextArea.setAttribute('data-noteid', e.target.parentElement.id);
-
-}
-
-/*** END OF EVENT HANDLERS FOR CLICK ACTIONS ***/
-
 function createClearButton() {
 
 	var clearButton = document.createElement('i');
@@ -247,11 +182,7 @@ function resetNotesView() {
 }
 
 function init() {
-	document.getElementById('createNoteButton').addEventListener('click', onCreateNoteButtonClicked, false);
-	document.getElementById('submitButton').addEventListener('click', onSubmitButtonClicked, false);
-	document.getElementById('cancelSubmitButton').addEventListener('click', onCancelButtonClicked, false);
-	document.getElementById('saveEditButton').addEventListener('click', onSaveButtonClicked, false);
-	document.getElementById('cancelEditButton').addEventListener('click', onCancelButtonClicked, false);
+	event_init();
 	bgPage.getPopupView();
 	bgPage.loadNotesFromDB();
 }
